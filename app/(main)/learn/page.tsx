5ab0a6a099388fd40d9f045d675e08b3253391e3
leonardodelira <leonardo.dlira@mercadolivre.com>
2024-03-27 09:14:43 -0300

    feat: seed script

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -18,7 +18,7 @@ const LearnPage = async () => {
         <div className="flex flex-row-reverse gap-[48px] px-6">
             <StickyWrapper>
                 <UserProgress
-                    activeCourse={{ title: userProgress.activeCourse.title, imageSrc: userProgress.activeCourse.imageSrc }}
+                    activeCourse={userProgress.activeCourse}
                     hearts={userProgress.hearts}
                     points={userProgress.points}
                     hasActiveSubscription={false}
