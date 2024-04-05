b67800ad94edfdee8aa5252f82e1046399ab9764
leonardodelira <leonardo.dlira@mercadolivre.com>
2024-04-05 16:47:00 -0300

    feat: promo in learn page

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -5,59 +5,65 @@ import { UserProgress } from "@/components/user-progress";
 import { getCourseProgress, getLessonPercentage, getUnits, getUserProgress, getUserSubscription } from "@/db/queries";
 import { redirect } from "next/navigation";
 import { Unit } from "./unit";
+import { Promo } from "@/components/promo";
+import { Quests } from "@/components/quests";
 
 const LearnPage = async () => {
-  const userProgressPromisse = getUserProgress();
-  const userSubscriptionPromisse = await getUserSubscription();
-  const courseProgressPromisse = getCourseProgress();
-  const lessonPercentagePromisse = getLessonPercentage();
-  const unitsPromisse = getUnits();
+    const userProgressPromisse = getUserProgress();
+    const userSubscriptionPromisse = await getUserSubscription();
+    const courseProgressPromisse = getCourseProgress();
+    const lessonPercentagePromisse = getLessonPercentage();
+    const unitsPromisse = getUnits();
 
-  const [userProgress, userSubscription, courseProgress, lessonPercentage, units] = await Promise.all([
-    userProgressPromisse,
-    userSubscriptionPromisse,
-    courseProgressPromisse,
-    lessonPercentagePromisse,
-    unitsPromisse,
-  ]);
+    const [userProgress, userSubscription, courseProgress, lessonPercentage, units] = await Promise.all([
+        userProgressPromisse,
+        userSubscriptionPromisse,
+        courseProgressPromisse,
+        lessonPercentagePromisse,
+        unitsPromisse,
+    ]);
 
-  if (!userProgress || !userProgress.activeCourse) {
-    redirect("/courses");
-  }
+    const isPro = !!userSubscription?.isActive;
 
-  if (!courseProgress) {
-    redirect("/courses");
-  }
+    if (!userProgress || !userProgress.activeCourse) {
+        redirect("/courses");
+    }
 
-  return (
-    <div className="flex flex-row-reverse gap-[48px] px-6">
-      <StickyWrapper>
-        <UserProgress
-          activeCourse={userProgress.activeCourse}
-          hearts={userProgress.hearts}
-          points={userProgress.points}
-          hasActiveSubscription={!!userSubscription?.isActive}
-        />
-      </StickyWrapper>
-      <FeedWrapper>
-        <Header title={userProgress.activeCourse.title} />
-        {units.map((unit) => (
-          <div key={unit.id} className="mb-10">
-            <Unit
-              key={unit.id}
-              id={unit.id}
-              order={unit.order}
-              title={unit.title}
-              description={unit.description}
-              lessons={unit.lessons}
-              activeLesson={courseProgress.activeLesson}
-              activeLessonPercentage={lessonPercentage}
-            />
-          </div>
-        ))}
-      </FeedWrapper>
-    </div>
-  );
+    if (!courseProgress) {
+        redirect("/courses");
+    }
+
+    return (
+        <div className="flex flex-row-reverse gap-[48px] px-6">
+            <StickyWrapper>
+                <UserProgress
+                    activeCourse={userProgress.activeCourse}
+                    hearts={userProgress.hearts}
+                    points={userProgress.points}
+                    hasActiveSubscription={!!userSubscription?.isActive}
+                />
+                {!isPro && <Promo />}
+                <Quests points={userProgress.points} />
+            </StickyWrapper>
+            <FeedWrapper>
+                <Header title={userProgress.activeCourse.title} />
+                {units.map((unit) => (
+                    <div key={unit.id} className="mb-10">
+                        <Unit
+                            key={unit.id}
+                            id={unit.id}
+                            order={unit.order}
+                            title={unit.title}
+                            description={unit.description}
+                            lessons={unit.lessons}
+                            activeLesson={courseProgress.activeLesson}
+                            activeLessonPercentage={lessonPercentage}
+                        />
+                    </div>
+                ))}
+            </FeedWrapper>
+        </div>
+    );
 };
 
 export default LearnPage;
