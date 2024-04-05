03c8136e4213b40068f6f26eb7148f28d2fd658a
leonardodelira <leonardo.dlira@mercadolivre.com>
2024-04-05 16:42:33 -0300

    feat: pages leaderboard and quests

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -2,21 +2,19 @@ import { MobileHeader } from "@/components/mobile-header";
 import { Sidebar } from "@/components/sidebar";
 
 type Props = {
-    children: React.ReactNode
-}
+    children: React.ReactNode;
+};
 
 const MainLayout = ({ children }: Props) => {
     return (
         <>
             <MobileHeader />
             <Sidebar className="hidden lg:flex" />
-            <main className="lg:pl-[256px] h-full pt-[50px] lg:pt-0">
-                <div className="max-w-[1056px] mx-auto pt-6 h-full">
-                    {children}
-                </div>
+            <main className="h-full pt-[50px] lg:pl-[256px] lg:pt-0">
+                <div className="mx-auto h-full max-w-[1056px] pt-6">{children}</div>
             </main>
         </>
-    )
-}
+    );
+};
 
 export default MainLayout;
